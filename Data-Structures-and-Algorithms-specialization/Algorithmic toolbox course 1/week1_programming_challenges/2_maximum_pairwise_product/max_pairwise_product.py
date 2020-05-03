cab714ad43684ffbd1c2bff804d3afd01649a3a3
X1AOX1A <1764400017@e.gzhu.edu.cn>
2020-05-03 16:53:38 +0800

    Week1: Done

--- Data-Structures-and-Algorithms-specialization/Algorithmic toolbox course 1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Data-Structures-and-Algorithms-specialization/Algorithmic toolbox course 1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -2,13 +2,13 @@
 
 
 def max_pairwise_product(numbers):
-    n = len(numbers)
-#just find biggest two numbers
+    #just find biggest two numbers
     num1 = max(numbers)
     numbers.remove(num1)
     num2 = max(numbers)
     return num1*num2
     
+#     n = len(numbers)
 #     max_product = 0
 #     for first in range(n):
 #         for second in range(first + 1, n):
@@ -17,7 +17,9 @@ def max_pairwise_product(numbers):
 
 #     return max_product
 
+
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
+
